5f22e9f6259a172a8cd838ed7552668dbfaf9cc8
Coran Mulcahy <coran.mulcahy@outlook.com>
2020-06-04 15:37:35 +1000

    Progress....

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# YelpCamp
+# WeCamp
+
 A large scale web application to view hand-picked campgrounds from all over the world!
--- app.js
+++ app.js
@@ -1,63 +1,58 @@
-const express = require("express");
-const app = express();
-const bodyParser = require("body-parser");
+const express = require("express"),
+   app = express(),
+   bodyParser = require("body-parser"),
+   mongoose = require("mongoose");
+
+mongoose.set("useNewUrlParser", true);
+mongoose.set("useFindAndModify", false);
+mongoose.set("useCreateIndex", true);
+mongoose.set("useUnifiedTopology", true);
+
+mongoose.connect("mongodb://localhost/WeCamp");
+
+// Schema set-up for mongoose
+const campgroundSchema = new mongoose.Schema({
+   name: String,
+   image: String,
+});
+
+const Campground = mongoose.model("Campground", campgroundSchema);
+
+// Campground.create(
+//    {
+//       name: "Halls Gap",
+//       image:
+//          "https://pixabay.com/get/52e5d7414355ac14f1dc84609620367d1c3ed9e04e50744075267bd69e49c6_340.jpg",
+//    },
+//    (err, campground) => {
+//       if (err) {
+//          console.log("Something went wrong...");
+//          console.log(err);
+//       } else {
+//          console.log("Added campground to database!");
+//          console.log(campground);
+//       }
+//    }
+// );
+
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
-
-var campgrounds = [
-   {
-      name: "Nagambie",
-      image:
-         "https://pixabay.com/get/55e8dc404f5aab14f1dc84609620367d1c3ed9e04e50744075277cd5914ac7_340.jpg",
-   },
-   {
-      name: "Halls Gap",
-      image:
-         "https://pixabay.com/get/52e5d7414355ac14f1dc84609620367d1c3ed9e04e50744075267bd69e49c6_340.jpg",
-   },
-   {
-      name: "Lakes Entrance",
-      image:
-         "https://pixabay.com/get/54e5dd424856ae14f1dc84609620367d1c3ed9e04e50744075267bd69e49c6_340.jpg",
-   },
-   {
-      name: "Nagambie",
-      image:
-         "https://pixabay.com/get/55e8dc404f5aab14f1dc84609620367d1c3ed9e04e50744075277cd5914ac7_340.jpg",
-   },
-   {
-      name: "Halls Gap",
-      image:
-         "https://pixabay.com/get/52e5d7414355ac14f1dc84609620367d1c3ed9e04e50744075267bd69e49c6_340.jpg",
-   },
-   {
-      name: "Lakes Entrance",
-      image:
-         "https://pixabay.com/get/54e5dd424856ae14f1dc84609620367d1c3ed9e04e50744075267bd69e49c6_340.jpg",
-   },
-   {
-      name: "Nagambie",
-      image:
-         "https://pixabay.com/get/55e8dc404f5aab14f1dc84609620367d1c3ed9e04e50744075277cd5914ac7_340.jpg",
-   },
-   {
-      name: "Halls Gap",
-      image:
-         "https://pixabay.com/get/52e5d7414355ac14f1dc84609620367d1c3ed9e04e50744075267bd69e49c6_340.jpg",
-   },
-   {
-      name: "Lakes Entrance",
-      image:
-         "https://pixabay.com/get/54e5dd424856ae14f1dc84609620367d1c3ed9e04e50744075267bd69e49c6_340.jpg",
-   },
-];
 
 app.get("/", (req, res) => {
    res.render("landing");
 });
 
 app.get("/campgrounds", (req, res) => {
-   res.render("campgrounds", { campgrounds: campgrounds });
+   // Get all campgrounds from database
+   Campground.find({}, (err, allCampgrounds) => {
+      if (err) {
+         console.log("Something went wrong...");
+         console.log(err);
+      } else {
+         res.render("campgrounds", { campgrounds: allCampgrounds });
+      }
+   });
+   // res.render("campgrounds", { campgrounds: campgrounds });
 });
 
 app.get("/campgrounds/new", (req, res) => {
@@ -69,11 +64,18 @@ app.post("/campgrounds", (req, res) => {
    const name = req.body.name;
    const image = req.body.image;
    const newCampground = { name: name, image: image };
-   campgrounds.push(newCampground);
-   // redirect back to campgrounds page
-   res.redirect("/campgrounds");
+   // Create a new campground and save to database
+   Campground.create(newCampground, (err, newlyCreated) => {
+      if (err) {
+         console.log("Something went wrong...");
+         console.log(err);
+      } else {
+         // redirect back to campgrounds page
+         res.redirect("/campgrounds");
+      }
+   });
 });
 
 app.listen(3000, (req, res) => {
-   console.log("The YelpCamp server has started! (port 3000)");
+   console.log("The WeCamp server has started! (port 3000)");
 });
